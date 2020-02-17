9e80ac5c7a3214413a1fc24e7f3bc3c2d033a570
xiankw <xiankw@hotmail.com>
2020-02-17 14:48:08 -0800

    Programs to log OpenRTK330 data

---
+++ openRTK_D.py
@@ -0,0 +1,41 @@
+
+"""
+Program to log DEBUG com data in binary from OpenRTK330 on Raspberry Pi
+Created on 2020-02-13
+@author: xiankw
+"""
+
+import serial
+import time
+import datetime
+from azureSAStest import push2azure
+                 
+ser = serial.Serial('/dev/ttyUSB2',460800,parity='N',bytesize=8,stopbits=1,timeout=None)  # need to confirm the port
+
+
+while True:
+    if ser.isOpen(): break
+
+print ('\Port is open now\n')
+ser.flushInput()
+
+
+fname = './data/openRTK-D-'
+fname += time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '.bin'
+fmode = 'wb'
+
+with open(fname,fmode) as outf:
+    while True:
+        try:
+            line = ser.readline()
+            outf.write(bytes(line))
+
+        except:
+            break
+
+    outf.close()
+
+time.sleep(10)
+print('Pushing to Azure ...')
+toCloud = push2azure()
+toCloud.push2AzureAsBlobs()
---
+++ openRTK_U.py
@@ -0,0 +1,37 @@
+
+"""
+Program to log USER com port data in binary from OpenRTK330 on Raspberry Pi
+Created on 2020-02-13
+@author: xiankw
+"""
+
+import serial
+import time
+import datetime
+from azureSAStest import push2azure
+                 
+ser = serial.Serial('/dev/ttyUSB0',460800,parity='N',bytesize=8,stopbits=1,timeout=None) # need to confirm the port
+
+
+while True:
+    if ser.isOpen(): break
+
+print ('\Port is open now\n')
+ser.flushInput()
+
+
+fname = './data/openRTK-U-'
+fname += time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '.bin'
+fmode = 'wb'
+
+with open(fname,fmode) as outf:
+    while True:
+        try:
+            line = ser.readline()
+            outf.write(bytes(line))
+
+        except:
+            break
+
+    outf.close()
+
